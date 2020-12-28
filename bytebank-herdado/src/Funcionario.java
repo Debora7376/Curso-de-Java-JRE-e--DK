931d18edf13c95ae68151c43f4b14d96e7f9df3b
debora7376 <devsouza.root@gmail.com>
2020-12-28 15:57:51 -0300

    Aula 4

--- bytebank-herdado/src/Funcionario.java
+++ bytebank-herdado/src/Funcionario.java
@@ -1,31 +1,36 @@
-
-public class Funcionario {
+// Não pode criar instancia dessa classe pq e abstrata
+public abstract class Funcionario {
 
 	private String nome;
 	private String cpf;
 	private double salario;
-	
-	 public double getBonificacao() {
-		 return this.salario * 0.05;
-	 }
-	
+
+	public double getBonificacao() {
+		return -1;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public double getSalario() {
 		return salario;
 	}
+
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
-	
+
 }
--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,24 +1,27 @@
-
 public class Gerente extends Funcionario {
 
-	private int senha; 
+	private int senha;
+	
+	@Override
+	public double getBonificacao() {
+	    return super.getBonificacao() + super.getSalario();
+	}
 	
 	public void setSenha(int senha) {
 		this.senha = senha;
 	}
-	
-	public boolean autentica (int senha) {
+
+	public boolean autentica(int senha) {
 		if (this.senha == senha) {
 			return true;
 		} else {
 			return false;
 		}
 		
+		public double getBonificacao() {
+			System.out.println("Chamando o metodo de bonificação do GERENTE");
+			return super.getSalario();
+		}
 	}
-	
-	 public double getBonificacao() {
-		 return super.getBonificacao() + super.getSalario();
-	 } 
-	
+
 }
- 
