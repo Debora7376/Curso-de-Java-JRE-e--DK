4b39a1dcf62a19f194d5d63e0cc4154cdf9c326e
debora7376 <devsouza.root@gmail.com>
2020-12-29 15:52:27 -0300

    Curso finalizado

--- Java Polimorfismo Entenda herança e interfaces/bytebank-herdado/src/AutenticacaoUtil.java
+++ Java Polimorfismo Entenda herança e interfaces/bytebank-herdado/src/AutenticacaoUtil.java
@@ -1,21 +1,13 @@
 
-public class Administrador extends Funcionario implements Autenticavel{
-	private int senha;
-	
-	@Override
-	public double getBonificacao() {
-		return 50;
-	}
-	
+public class AutenticacaoUtil {
 
-	@Override
+	private int senha; 
+	
     public void setSenha(int senha){
         this.senha = senha;
     }
 
-    @Override
     public boolean autentica(int senha){
-
         if(this.senha == senha){
             return true;
         } else {
